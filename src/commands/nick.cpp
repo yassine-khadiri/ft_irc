52f1fd9de439d0a194eec43e1affdd44b484c6d9
rgatnaou <gatnaouiradwane@gmail.com>
2023-06-14 18:51:34 +0100

    merge some changes

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -6,7 +6,7 @@
 /*   By: rgatnaou <rgatnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 18:45:18 by ykhadiri          #+#    #+#             */
-/*   Updated: 2023/06/11 13:56:45 by rgatnaou         ###   ########.fr       */
+/*   Updated: 2023/06/14 17:59:05 by rgatnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,40 @@
 void Command::nickCommand()
 {
 	std::string hostname = getMachineHostName();
+	std::string nickname = (_client.getNickname() == "") ? "*" : _client.getNickname();
 
 	if(this->_client.getPassword() == "")
 	{
-		sendReply(":"+ getMachineHostName() +" ERROR * :You must enter a password first\r\n");
+		sendReply(":" + hostname + " ERROR * : You must enter a password first\r\n");
 		return ;
 	}
 	if (this->_args.size() != 1 || this->_args[0] == "")
-		sendReply(":"+ getMachineHostName() +" 431 " + _client.getNickname() + ": NICK <nickname>\r\n");
+		sendReply(":" + hostname + " 431 " + nickname + " : NICK <nickname>\r\n");
 	else if (this->_args[0].find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_") != std::string::npos)
-		sendReply(":"+ getMachineHostName() +" 432 " + _client.getNickname() + ": Nickname invalid\r\n");
+		sendReply(":" + hostname + " 432 " + nickname + " : Nickname invalid\r\n");
 	else if(nickExist(this->_args[0]) != -1)
-		sendReply(":"+ getMachineHostName() +" 433 " + _client.getNickname()+ " : Nickname already in use\r\n");
+		sendReply(":" + hostname + " 433 " + nickname + " : Nickname already in use\r\n");
 	else
 	{
 		if (_client.getNickname() == "" && _client.getUsername() != "")
 		{
-			this->_client.setNickname(this->_args[0]);  
 			sendReply(":"+ hostname + " 001 " + this->_args[0] + " :Welcome to the Internet Relay Network " + this->_args[0] + "!\r\n");
-			sendReply(":"+ hostname + " 002 " + this->_args[0] + " :Your host is "+ getMachineHostName() +", running version 0.1\r\n");
+			sendReply(":"+ hostname + " 002 " + this->_args[0] + " :Your host is "+ hostname +", running version 0.1\r\n");
 			sendReply(":"+ hostname + " 003 " + this->_args[0] + " :This server was created 2019-10-10\r\n");
-			sendReply(":"+ hostname + " 004 " + this->_args[0] + " :"+ getMachineHostName() +" 0.1\r\n");
+			sendReply(":"+ hostname + " 004 " + this->_args[0] + " :"+ hostname +" 0.1\r\n");
 			sendReply(":"+ hostname + " 251 " + this->_args[0] + " :There are 1 users and 1 server\r\n");
-			sendReply(":"+ getMachineHostName() +" 372 " + _client.getNickname() +  " :         	       _       _                            ___ ____   ____   ____                           \r\n");
-			sendReply(":"+ getMachineHostName() +" 372 " + _client.getNickname() +  " :              | | ___ (_)_ __     ___  _   _ _ __  |_ _|  _ \\ / ___| / ___|  ___ _ ____   _____ _ __ \r\n");
-			sendReply(":"+ getMachineHostName() +" 372 " + _client.getNickname() +  " :           _  | |/ _ \\| | '_ \\   / _ \\| | | | '__|  | || |_) | |     \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\r\n");
-			sendReply(":"+ getMachineHostName() +" 372 " + _client.getNickname() +  " :          | |_| | (_) | | | | | | (_) | |_| | |     | ||  _ <| |___   ___) |  __/ |   \\ V /  __/ |   \r\n");
-			sendReply(":"+ getMachineHostName() +" 372 " + _client.getNickname() +  " :           \\___/ \\___/|_|_| |_|  \\___/ \\__,_|_|    |___|_| \\_\\____| |____/ \\___|_|    \\_/ \\___|_|   \r\n");
+			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :         	       _       _                            ___ ____   ____   ____                           \r\n");
+			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :              | | ___ (_)_ __     ___  _   _ _ __  |_ _|  _ \\ / ___| / ___|  ___ _ ____   _____ _ __ \r\n");
+			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :           _  | |/ _ \\| | '_ \\   / _ \\| | | | '__|  | || |_) | |     \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\r\n");
+			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :          | |_| | (_) | | | | | | (_) | |_| | |     | ||  _ <| |___   ___) |  __/ |   \\ V /  __/ |   \r\n");
+			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :           \\___/ \\___/|_|_| |_|  \\___/ \\__,_|_|    |___|_| \\_\\____| |____/ \\___|_|    \\_/ \\___|_|   \r\n");
 			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :                        Please enjoy your stay!\r\n");
 			sendReply(":"+ hostname + " 372 " + this->_args[0] + " :you can use bot command (BOT) to get time && jokes && nokta!\r\n");
 			sendReply(":"+ hostname + " 376 " + this->_args[0] + " :Made by hbouqssi && ykhadiri && rgatnaou\r\n");
 			this->_client.setIsRegistered(true);
 		}
 		else if(_client.getIsRegistered())
-			communClients(":" + _client.getNickname() + "!" + _client.getUsername() + "@"+ getMachineHostName() +"" + " NICK " + this->_args[0] + "\r\n"); 
+			communClients(":" + _client.getNickname() + "!" + _client.getUsername() + "@" + hostname + " NICK " + this->_args[0] + "\r\n"); 
 		this->_client.setNickname(this->_args[0]);
 	}
 };
--- src/commands/user.cpp
+++ src/commands/user.cpp
@@ -6,7 +6,7 @@
 /*   By: rgatnaou <rgatnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 18:46:32 by ykhadiri          #+#    #+#             */
-/*   Updated: 2023/05/30 16:04:06 by rgatnaou         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:01:39 by rgatnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,34 @@
 
 void Command::userCommand()
 {
+	std::string nickname = (_client.getNickname() == "") ? "*" : _client.getNickname();
+	
 	if(this->_client.getPassword().empty())
 	{
-		sendReply(":"+ getMachineHostName() + " ERROR * :You must enter a password first\r\n");
+		sendReply(":" + getMachineHostName() + " ERROR * :You must enter a password first\r\n");
 		return ;
 	}
 	if (this->_args.size() != 4)
-		sendReply(":"+ getMachineHostName() +" 461 " + _client.getNickname() + ": USER <username> <hostname> <servername> <realname>\r\n");
+		sendReply(":" + getMachineHostName() + " 461 " + nickname + " :USER <username> <hostname> <servername> <realname>\r\n");
 	else if (this->_client.getUsername() != "" && this->_client.getNickname() != "")
-		sendReply(":"+ getMachineHostName() +" 462 " + _client.getNickname() + ":You are already registered\r\n");
+		sendReply(":" + getMachineHostName() + " 462 " + nickname + " :You are already registered\r\n");
 	else
 	{
 		if (_client.getNickname() != "" && _client.getUsername() == "")
 		{
-			sendReply(":" + getMachineHostName() + " 001 " + _client.getNickname() + " :Welcome to the Internet Relay Network " + _client.getNickname() + "!\r\n");
-			sendReply(":" + getMachineHostName() + " 002 " + _client.getNickname() + " :Your host is " + getMachineHostName() + ", running version 0.1\r\n");
-			sendReply(":" + getMachineHostName() + " 003 " + _client.getNickname() + " :This server was created 2019-10-10\r\n");
-			sendReply(":" + getMachineHostName() + " 251 " + _client.getNickname() + " :There are 1 users and 1 server\r\n");
-			sendReply(":" + getMachineHostName() + " 004 " + _client.getNickname() + " :" + getMachineHostName() + " 0.1\r\n");                                                                                 
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :         	       _       _                            ___ ____   ____   ____                           \r\n");
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :              | | ___ (_)_ __     ___  _   _ _ __  |_ _|  _ \\ / ___| / ___|  ___ _ ____   _____ _ __ \r\n");
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :           _  | |/ _ \\| | '_ \\   / _ \\| | | | '__|  | || |_) | |     \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\r\n");
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :          | |_| | (_) | | | | | | (_) | |_| | |     | ||  _ <| |___   ___) |  __/ |   \\ V /  __/ |   \r\n");
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :           \\___/ \\___/|_|_| |_|  \\___/ \\__,_|_|    |___|_| \\_\\____| |____/ \\___|_|    \\_/ \\___|_|   \r\n");
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :                        Please enjoy your stay!\r\n");
-			sendReply(":" + getMachineHostName() + " 372 " + _client.getNickname() + " :you can use bot command (BOT) to get time && jokes && nokta!\r\n");
-			sendReply(":" + getMachineHostName() + " 376 " + _client.getNickname() + " :Made by hbouqssi && ykhadiri && rgatnaou\r\n");
-
+			sendReply(":" + getMachineHostName() + " 001 " + nickname + " :Welcome to the Internet Relay Network " + nickname + "!\r\n");
+			sendReply(":" + getMachineHostName() + " 002 " + nickname + " :Your host is " + getMachineHostName() + ", running version 0.1\r\n");
+			sendReply(":" + getMachineHostName() + " 003 " + nickname + " :This server was created 2019-10-10\r\n");
+			sendReply(":" + getMachineHostName() + " 251 " + nickname + " :There are 1 users and 1 server\r\n");
+			sendReply(":" + getMachineHostName() + " 004 " + nickname + " :" + getMachineHostName() + " 0.1\r\n");                                                                                 
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :         	       _       _                            ___ ____   ____   ____                           \r\n");
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :              | | ___ (_)_ __     ___  _   _ _ __  |_ _|  _ \\ / ___| / ___|  ___ _ ____   _____ _ __ \r\n");
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :           _  | |/ _ \\| | '_ \\   / _ \\| | | | '__|  | || |_) | |     \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\r\n");
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :          | |_| | (_) | | | | | | (_) | |_| | |     | ||  _ <| |___   ___) |  __/ |   \\ V /  __/ |   \r\n");
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :           \\___/ \\___/|_|_| |_|  \\___/ \\__,_|_|    |___|_| \\_\\____| |____/ \\___|_|    \\_/ \\___|_|   \r\n");
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :                        Please enjoy your stay!\r\n");
+			sendReply(":" + getMachineHostName() + " 372 " + nickname + " :you can use bot command (BOT) to get time && jokes && nokta!\r\n");
+			sendReply(":" + getMachineHostName() + " 376 " + nickname + " :Made by hbouqssi && ykhadiri && rgatnaou\r\n");
 			this->_client.setIsRegistered(true);
 		}
 		this->_client.setUsername(this->_args[0]); 
