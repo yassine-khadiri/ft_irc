22f56e634e60b59821d2ab255f822cec2c93e099
Hamza Bouqssim <hbouqssi@e1r7p4.1337.ma>
2023-06-17 18:24:23 +0100

    finished

--- src/commands/bot.cpp
+++ src/commands/bot.cpp
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   bot.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ykhadiri <ykhadiri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/24 18:44:22 by ykhadiri          #+#    #+#             */
-/*   Updated: 2023/06/16 20:01:48 by ykhadiri         ###   ########.fr       */
+/*   Created: 2023/06/17 18:22:24 by hbouqssi          #+#    #+#             */
+/*   Updated: 2023/06/17 18:23:23 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/Command.hpp"
+
+size_t WriteCallback( void* contents, size_t size, size_t nmemb, std::string* output )
+{
+    size_t totalSize = size * nmemb;
+    output->append(static_cast<char*>(contents), totalSize);
+    return totalSize;
+};
 
 std::string getTime()
 {
@@ -31,6 +38,11 @@ std::string getJokeQuote()
 	nokat_7amdin.push_back("Hada wahd 9ta3 chanti .. ohowa ykhrej lih zaft");
 	nokat_7amdin.push_back("hadi wahed nokta b me9lob iwa dehako o ne3awedalkom hhhhhhh loool");
 	nokat_7amdin.push_back("aaa ch77aaal 7amd hhhhhhhhh");
+	nokat_7amdin.push_back("gal lik hada wa7ed khyna taji lbar wtay demandé 3la 2 dial 9ra3i wa 2 dial kissane whwa galesse ghir bou7dou");
+	nokat_7amdin.push_back("wahed almarra le garçon darro rasso wa mcha sawlo :3lache nta dima kate demandé 3la 2 9ra3i wa 2 kissane wtatb9a tchrab mnhoum b 2 ,....");
+	nokat_7amdin.push_back("hada wahad lma3gaz, mcha lpissri, galih; kayen lhalib, galih la, ohowa yamchi 3and pissri khor galih onta ");
+	nokat_7amdin.push_back("hada wahd tjawaj gawrya charfa wa jaw lmaghrib wa kay doro f chari3 chwya hya tih fi trotoire whowa ygolo wahd khaina haz haz assahbi lwrake dyalak taho");
+	nokat_7amdin.push_back("galak hada wahad rajal mcha ydrab telephone whewa yadi m3ah la3ssa");
     std::srand(std::time(0));
     int randIndex = std::rand() % nokat_7amdin.size();
     return nokat_7amdin[randIndex];
@@ -46,13 +58,50 @@ void Command::botCommand()
 	std::stringstream channelSplitter(this->_args[0]);
 	std::string cmd = channelSplitter.str();
 
-	if (cmd != "time" && cmd != "nokta")
+	if (cmd != "time" && cmd != "nokta"  && cmd != "jokes")
 	{
-		sendReply("300 RPL_NONE: Available Bots Now : [time - nokta]\r\n");
+		sendReply("300 RPL_NONE: Available Bots Now : [time - nokta - jokes]\r\n");
 		return ;
 	}
 	if (cmd == "time")
 		sendReply("300 RPL_NONE :" + getTime());
 	if (cmd == "nokta")
-		sendReply("300 RPL_NONE :" + getJokeQuote() + "\r\n");
+		sendReply("300 RPL_NONE :" + getJokeQuote() + "\r\n");	
+	if (cmd == "jokes")
+	{
+		std::string url = "https://api.api-ninjas.com/v1/jokes?limit"; //add your url
+
+		// Initialize cURL
+		CURL* curl = curl_easy_init();
+		if (curl)
+		{
+			// Set the URL
+			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+			// Set the headers
+			struct curl_slist* headers = nullptr;
+			headers = curl_slist_append(headers, "X-Api-Key: KjtJWjMZe7WLG0ucyGyHvcOuhssaEopZZKXnCUhu"); //add your key
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+			// Set the response callback
+			std::string response;
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+			// Perform the request
+			CURLcode res = curl_easy_perform(curl);
+			if (res != CURLE_OK)
+				std::cerr << "Error: " << curl_easy_strerror(res) << std::endl;
+			else
+			{
+				std::string clean = response.erase(response.size() - 3);
+				std::string output = clean.substr(11, -1);
+				sendReply("300 RPL_NONE: " + output + "\r\n");
+			}
+
+			// Clean up
+			curl_easy_cleanup(curl);
+			curl_slist_free_all(headers);
+		}
+	} 
 };
